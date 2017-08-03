af11ed59eb2d75ef23835a8de512a920019ca6f6
Khac Huy Le <lekhachuy08@gmail.com>
2017-08-03 20:14:19 +1000

    Fix #34 Add advertisement

--- game/src/index.js
+++ game/src/index.js
@@ -4,3 +4,17 @@ import './index.css'
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+const loadAdMob = () => {
+  if (window.AdMob) {
+    const adId = /(android)/i.test(navigator.userAgent)
+      ? 'ca-app-pub-6654289360326490/9422160073'
+      : 'ca-app-pub-6654289360326490/2114515697'
+    window.AdMob.createBanner({
+      adId,
+      position: window.AdMob.AD_POSITION.TOP_CENTER,
+      autoShow: true
+    })
+  }
+}
+document.addEventListener('deviceready', loadAdMob, false)
