4acb44e68aec2abc093cc79f34b990220fe2fa7f
Ngoc Dao <ngocdaothanh@gmail.com>
2017-08-09 16:18:42 +1000

    <script type="text/javascript" src="cordova.js"></script>

--- game/src/index.js
+++ game/src/index.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-const initApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
+const loadAdmob = () => {
   if (window.AdMob) {
     // gplinh@gmail.com
-    const adId = /(android)/i.test(navigator.userAgent)
+    const adId = /android/i.test(navigator.userAgent)
       ? 'ca-app-pub-6654289360326490/9422160073'
       : 'ca-app-pub-6654289360326490/2114515697'
 
@@ -28,7 +28,5 @@ const initApp = () => {
 }
 
 if (/(ipad|iphone|ipod|android)/i.test(navigator.userAgent)) {
-  document.addEventListener('deviceready', initApp, false)
-} else {
-  initApp()
+  document.addEventListener('deviceready', loadAdmob, false)
 }
